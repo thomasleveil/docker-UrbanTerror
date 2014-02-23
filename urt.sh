bdefac1ca8e078ffe36c98eabdcf5fee0f09f6cb
Thomas LÉVEIL <thomasleveil@gmail.com>
2014-02-23 21:42:21 +0100

    add fs_game and com_hunkmegs to the command line starting UrT

--- urt.sh
+++ urt.sh
@@ -2,5 +2,5 @@
 
 ## run the UrT server
 echo "===== running UrT server ======"
-exec /sbin/setuser urt /home/urt/UrbanTerror42/Quake3-UrT-Ded.x86_64 +set fs_homepath / +set dedicated 2 +set net_port 27960 +exec server.cfg 2>&1
+exec /sbin/setuser urt /home/urt/UrbanTerror42/Quake3-UrT-Ded.x86_64 +set fs_game q3ut4 +set fs_homepath / +set dedicated 2 +set net_port 27960 +exec server.cfg +set com_hunkmegs 128 2>&1
 
