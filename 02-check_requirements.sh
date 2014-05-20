0aef1a1d497bed2ee7172468c42aa5fa833d4dab
Thomas LEVEIL <thomasleveil@gmail.com>
2014-05-20 01:04:08 +0000

    create a default UrT server config file if no config file is found

--- 02-check_requirements.sh
+++ 02-check_requirements.sh
@@ -2,7 +2,7 @@
 
 ## make sure we got a config file
 if [ ! -f "/q3ut4/server.cfg" ]; then
-    echo "ERROR: expecting docker container to have file /q3ut4/server.cfg" 1>&2 
-    exit 1
+    echo "WARNING: no UrT server config file found, copying default config" 1>&2 
+    sed -r 's/(^set\s+sv_hostname\s+).+/\1"docker-'$HOSTNAME'"/' /home/urt/UrbanTerror42/q3ut4/server_example.cfg > /q3ut4/server.cfg
 fi
 
