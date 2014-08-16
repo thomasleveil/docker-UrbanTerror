2591b4896b4a05d313a893b0d8701eb6c81542af
Thomas LÉVEIL <thomasleveil@gmail.com>
2014-08-16 12:08:37 +0000

    check that a volume for /q3ut4 exists

--- 02-check_requirements.sh
+++ 02-check_requirements.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+## make sure /q3ut4 exists
+if [ ! -d "/q3ut4" ]; then
+    cat <<-EOT 1>&2
+	
+	ERROR: You must run the container with a volume on /q3ut4.
+	
+	EOT
+	exit 3
+fi
+
 ## make sure we got a config file
 if [ ! -f "/q3ut4/server.cfg" ]; then
     echo "WARNING: no UrT server config file found, copying default config" 1>&2 
